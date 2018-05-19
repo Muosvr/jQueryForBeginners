45c7a05f69d72cd27adedbfdb7ae8aee48dd2f9c
Jason Wu <lujason2015@gmail.com>
2018-05-19 12:59:26 -0400

    #1-8

---
+++ scripts/script.js
@@ -0,0 +1,44 @@
+//#5 Element selectors
+// $('h3').css({border: "3px solid blue"});
+
+// Class selectors
+// $('.wrapper').css({border: "3px solid red"});
+
+// Id selectors
+// $('#clients').css({border: "3px solid yellow"});
+
+//#6 jQuery Filters
+
+// grab the first li item in the header inside nav and create borders around them
+// $("header nav li:first").css({border:"2px solid pink"});
+// $("header nav li:last").css({border:"3px solid pink"});
+
+//#7 Traversing the DOM with jQuery
+
+//next
+//$("#contact-methods").next().css({border: "3px dotted black"});
+
+//prev
+// $("#social-nav").prev().css({border: "3px solid black"});
+
+//Other methods: .parent(), .parentAll(), .children(), .find(), .closest()
+//.find()-better performance,
+//.closest() - find the nearest parent that matches the element
+//.parentAll() - final all parents
+
+// jQuery Chaining
+// $("#contact-methods").css({border: "2px solid red"}) //find the element with Id of "contact-methods" and create a 2 px red border around it
+//     .next().css({border: "2px solid green"}) //find the next element down the line and create a 2 px solid green border around it
+//     .closest("section").css({border: "2px solid blue"}); // find the closest parent with the tag "section" and create a 2 px blue border around it
+
+//#9 Adding Content using jQuery
+// var tweet = "<div style='margin: 20px 0; padding: 10px; background: #eee;'>The big fight live: Ham vs Cheese!</div>";
+
+// $("#tweets div").append(tweet);
+
+//.append() adds content to bottom of the element
+//.prepend() adds content to top of element
+//.before() add content before element
+//.after() add content after element
+//.html() changes the whole html of the element
+//.text() changes the text of an element
